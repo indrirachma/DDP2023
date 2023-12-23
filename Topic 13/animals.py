99355a096820309883c81fb4159401ae887d3b7b
Indri Rachma <indr23101ti@student.nurulfikri.ac.id>
2023-12-23 21:36:05 +0700

    update

--- Topic 13/animals.py
+++ Topic 13/animals.py
@@ -11,3 +11,28 @@ class Animals:
         print("Makanan \t:", self.makanan)
         print("Hidup \t\t:", self.hidup)
         print("Berkembang Biak :", self.berkembangbiak)
+
+class Badak(Animals):
+    def one(self):
+        print("Badak adalah hewan langka.")
+
+class Ikan(Animals):
+    def two(self):
+        print("Hiu merupakan hewan purba.")
+
+class Ular(Animals):
+    def three(self):
+        print("Ular adalah hewan yang berbisa.")
+
+# akses method
+a = Badak("Badak", "Herbivora", "Darat", "Vivipar",)
+a.info()
+a.one()
+
+b = Ikan("Ikan", "Omnivora", "Air", "Ovovivipar")
+b.info()
+b.two()
+
+c = Ular("Ular", "Karnivora", "Amfibi", "Ovovivipar")
+c.info()
+c.three()
